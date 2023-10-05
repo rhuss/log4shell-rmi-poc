ddb8e60e4d58b385c7283083de4658d08a5f2137
Roland Huß <rhuss@redhat.com>
2023-10-05 18:57:06 +0200

    Renamed

--- vulnerable-log4j-app/src/main/java/knativecon2023/zerotrust/WebApp.java
+++ vulnerable-log4j-app/src/main/java/knativecon2023/zerotrust/WebApp.java
@@ -1,7 +1,5 @@
 package knativecon2023.zerotrust;
 
-import java.io.IOException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
