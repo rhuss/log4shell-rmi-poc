f649d4a6d398033353af2033613a2c924f571fad
Roland Huß <rhuss@redhat.com>
2023-10-06 08:01:11 +0200

    fixed

--- log4jshell-rmi-server/startRmiServer.sh
+++ log4jshell-rmi-server/startRmiServer.sh
@@ -2,7 +2,7 @@
 mkdir -p target/classes/static
 cp -r web/* target/classes/static
 tar -czvf site.tgz target/classes/static/
-if ! base64 site.tgz -w0 > site.tgz.base64 >/dev/null 2>&1; then
+if ! base64 site.tgz -w0 > site.tgz.base64 2>/dev/null; then
   base64 -i site.tgz -o site.tgz.base64
 fi
 
